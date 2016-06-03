066e763dcff2c9ba389eaa7fedc3f664101cb692
Exadra37 <exadra37@gmail.com>
2016-06-03 22:00:29 +0100

    File System Trait - Created with functions to be sourced by other bash scripts.

---
+++ sourcing/file-system-trait.source.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# @author Exadra37(Paulo Silva) <exadra37ingmailpointcom>
+# @since  2016/05/21
+# @link   http://exadra37.com
+
+set -e
+
+#################################################################################################################################################################
+# Functions
+#################################################################################################################################################################
+
+    function getScriptDir
+    {
+        local script_dir
+
+        script_dir=$( cd "$( dirname "$0" )" && pwd )
+
+        echo "${script_dir}"
+    }
+
+    function Mkdir
+    {
+        local path="$1"
+
+        mkdir "${path}"
+    }
+
+    function Create_Dir
+    {
+        local path="$1"
+
+        mkdir -p "${path}"
+    }
+
+    function Touch_File
+    {
+        local file="$1"
+
+        touch "${file}"
+    }
+
+    function Create_File
+    {
+        local file="$1"
+
+        [ -f "${file}" ] || Touch_File "${file}"
+    }
